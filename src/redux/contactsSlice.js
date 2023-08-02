6cee75f2974f69c7fc12a09c1df06d49bf2f151c
IraMira333 <irinat333@gmail.com>
2023-08-02 22:24:44 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,9 +3,9 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    contactsInfo: [
-      { id: '-QvnxS2I4k_vf34CIkfPa', name: 'Avea Trotter', number: 234532 },
-    ],
+    contactsInfo: [],
+    isLoading: false,
+    error: null,
   },
   reducers: {
     addTask: {
