abed8a33a904b92bea65624ed650a5ad09faa985
IraMira333 <irinat333@gmail.com>
2023-08-03 15:26:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,22 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from './App.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
+import * as contactsOperations from '../redux/operations';
+import { useEffect } from 'react';
 
 export default function App() {
-  const contacts = useSelector(getContacts);
+  const entities = useSelector(getContacts);
   const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(contactsOperations.fetchContacts());
+  }, [dispatch]);
 
   function getFilterContacts() {
-    return contacts.filter(contact =>
+    return entities.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   }
@@ -19,10 +26,8 @@ export default function App() {
     <div className={css.phonebook}>
       <h2>Phonebook</h2>
       <ContactForm />
-
       <h2>Contacts</h2>
-
-      {contacts.length > 0 ? (
+      {entities.length > 0 ? (
         <Filter />
       ) : (
         <p className={css.noContact}>You don't have any contact yet</p>
