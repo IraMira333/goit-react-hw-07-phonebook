16052902d41f8db839ef238078a8a40164d36855
IraMira333 <irinat333@gmail.com>
2023-08-05 00:33:20 +0300

    Update contacts_API.js

--- src/services/contacts_API.js
+++ src/services/contacts_API.js
@@ -4,6 +4,6 @@ axios.defaults.baseURL = 'https://64ca4e55700d50e3c704af47.mockapi.io/contacts';
 
 export async function fetchContacts() {
   const { data } = await axios.get('/contacts');
-  console.log(data);
+
   return data;
 }
