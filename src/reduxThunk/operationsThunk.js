a78d1f2db05eda79a00857827ee8f7ede34000e9
IraMira333 <irinat333@gmail.com>
2023-08-05 00:30:51 +0300

    Update operationsThunk.js

--- src/reduxThunk/operationsThunk.js
+++ src/reduxThunk/operationsThunk.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as contactsAPI from '../services/contacts_API';
+import axios from 'axios';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
@@ -7,6 +8,30 @@ export const fetchContacts = createAsyncThunk(
     try {
       const contacts = await contactsAPI.fetchContacts();
       return contacts;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, phone }, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/contacts', { name, phone });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const removeContact = createAsyncThunk(
+  'contacts/removeContact',
+  async (contactId, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.delete(`/contacts/${contactId}`);
+      return data;
     } catch (error) {
       return rejectWithValue(error.message);
     }
