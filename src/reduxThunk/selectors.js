755bf01b4e0f2af366602e7691cb5e077219998c
IraMira333 <irinat333@gmail.com>
2023-08-05 16:29:36 +0300

    Update selectors.js

--- src/reduxThunk/selectors.js
+++ src/reduxThunk/selectors.js
@@ -1,2 +1,12 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.contactsInfo;
 export const selectFilter = state => state.filter.filter;
+
+export const selectFilteredContacts = createSelector(
+  [state => state.contacts.contactsInfo, state => state.filter.filter],
+  (contacts, filter) =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    )
+);
